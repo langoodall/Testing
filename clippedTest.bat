5b357fe39e9a3bf590aeed020908fb61af71b30f
Louis Goodall <159187174+langoodall@users.noreply.github.com>
2024-07-02 09:28:07 -0400

    Add files via upload

---
+++ clippedTest.bat
@@ -0,0 +1,12 @@
+rem Batch File to Run a Scenario 
+
+rem The 'rem' keyword indicates that this is a remark
+
+
+call landis-ii-7 clippedTest_scenario.txt
+
+
+rem call landis-ii scenario-single-cell.txt
+
+
+pause
